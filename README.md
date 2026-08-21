2fb35f7a42c23a71636b29bb36f5bd4c84f3928e
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-21 19:28:57 +0530

    Sum.py
    A function is a block of reusable code that performs a specific task.
    
    def function_name(parameters):
        # code
    
    Step	Statement	Value
    1	add(10, 20)	a = 10, b = 20
    2	sum = a + b	sum = 10 + 20 = 30
    3	print(sum)	30

--- README.md
+++
@@ -1,2 +0,0 @@
-# Function_recursion_workspace
-Python programs and practice questions for beginners
---
+++ Sum.py
@@ -0,0 +1,8 @@
+# sum = 10+20 = 30
+def add (a , b):
+    sum = a+b
+    print(sum)
+    
+add(10 , 20)
+
+
